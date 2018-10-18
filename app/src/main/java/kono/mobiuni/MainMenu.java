12dc3a4d1faa2c251baa6757e55426bbd2e68ca1
Yustam0ment <konolearning@gmail.com>
2018-10-18 21:54:51 +0200

    Init

---
+++ app/src/main/java/kono/mobiuni/MainMenu.java
@@ -0,0 +1,18 @@
+package kono.mobiuni;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+import kono.mobiuni.View.LogInActivity;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main_menu);
+    }
+}
